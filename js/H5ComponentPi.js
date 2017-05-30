7e375d961dc9fca5cd9584d041448d7657d0ac29
FlynnScif <hydehyde111@163.com>
2017-05-30 23:20:55 +0800

    饼图-完成

--- js/H5ComponentPi.js
+++ js/H5ComponentPi.js
@@ -33,7 +33,7 @@ var H5ComponentPi = function ( name,cfg ) {
 	$(cns).css('zIndex',2);
 	component.append(cns);
 
-	var colors = ['red','green','blue','yellow','gray'];//备用颜色
+	var colors = ['red','green','blue','#a00','orange'];//备用颜色
 	var sAngel = 1.5*Math.PI;//设置开始的角度在12点位置
 	var eAngel = 0;//结束角度
 	var aAngel = Math.PI*2;//100%的圆角度的角度  2pi =360 
@@ -52,12 +52,51 @@ var H5ComponentPi = function ( name,cfg ) {
 		ctx.fillStyle=color;
 		ctx.strokeStyle=color;
 		ctx.lineWidth =.1;
+
+
 		ctx.moveTo(r,r);
 		ctx.arc(r,r,r,sAngel,eAngel);
 		ctx.fill();
 		ctx.stroke();
+		//画完一个后重新定义起启角度
+		sAngel=eAngel;
 
-		sAngel=eAngel;//画完一个后重新定义起启角度
+		//加入所有的项目文本及百分比
+		var text = $('<div class ="text">');
+		text.text (cfg.data[i][0]);
+		var per = $('<div class ="per">');
+		per.text = (cfg.data[i][0]*100+'%');
+		text.append(per);
+
+		//定位文本
+		//var x = r +Math.sin(sAngel)*r;
+		//var y = r +Math.cos(sAngel)*r;
+		var x = r +Math.sin( Math.PI - sAngel)*r;
+		var y = r +Math.cos( Math.PI - sAngel)*r;
+
+		//text.css('left',x/2).css('top',y/2);
+
+		if(x> w/2){
+			text.css('left',x/2);
+		}else{
+			text.css('right',(w-x)/2);
+		}
+
+				if(y> h/2){
+			text.css('top',y/2);
+		}else{
+			text.css('buttom',(h-y)/2);
+		}
+
+		//字体颜色
+		if(cfg.data[i][2]){
+			text.css('color',cfg.data[i][2]);
+		}
+
+		//在动画显示完全后再显示文字
+		text.css('opacity',0);
+
+		component.append(text);
 
 	}
 
@@ -84,8 +123,9 @@ var H5ComponentPi = function ( name,cfg ) {
 		ctx.beginPath();
 		ctx.moveTo(r,r);
 		if(per <= 0){
-			ctx.arc(r,r,r,0,2*Math.PI,true);
 			//最后一个参数是为了逆行
+			ctx.arc(r,r,r,0,2*Math.PI,true);
+			component.find('.text').css('opacity',0);
 		}else{
 			ctx.arc(r,r,r,sAngel,sAngel+2*Math.PI*per,true);
 			//最后一个参数是为了逆行
@@ -93,6 +133,11 @@ var H5ComponentPi = function ( name,cfg ) {
 		}
 		ctx.fill();
 		ctx.stroke();
+
+		//动画执行完后显示文字
+		if(per >= 1){
+			component.find('.text').css('opacity',1);
+		}
 	}
 	draw(0);
 
